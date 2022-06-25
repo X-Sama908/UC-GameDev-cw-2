c75a08294fafa7d07afeb085a760b78672fc780e
X-Sama908 <ashketchum_2011@hotmail.com>
2022-06-25 19:47:29 +0300

    Done

---
+++ HEROandVillan2/HEROandVillan2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEROandVillan2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string HeroName = "All Might";
+            int heroPower = 20000;
+            
+
+            string villainName = "All For One";
+            int villainPower = 1000;
+
+
+            if (heroPower < villainPower)
+            {
+                Console.WriteLine(" villan will try to increase his power!!! ");
+
+
+            }// end if 
+
+            else if (heroPower > villainPower)
+
+            {
+                Console.WriteLine("The hero is always greater :D !");
+
+            }
+
+            else 
+            {
+
+                Console.WriteLine("that's not possible");
+            
+            }
+
+
+
+            }
+
+            }
+        }
+
---
+++ Speed101/Speed101/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speed101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float playerSpeed = 2.7f;
+
+            void SetSpeed(float x)
+            {
+                Console.WriteLine(playerSpeed);
+
+                playerSpeed = x;
+
+                Console.WriteLine(playerSpeed);
+
+            }
+
+            SetSpeed(2.5f);
+
+            
+            void CompareSpeed(float old,float new2)
+
+            { 
+
+                if(old > new2)
+
+                {
+                    Console.WriteLine(old);
+
+                }
+
+                else if (new2>old)
+                {
+                    Console.WriteLine(new2);
+
+                }
+
+            }
+            CompareSpeed(2.7f, 2.5f);
+
+
+        }
+    }
+}
